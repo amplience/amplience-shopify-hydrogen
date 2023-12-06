2f44e91c0e33765bad56697ad6ec2a63c2f7e010
Réza Kalfane <reza.kalfane@gmail.com>
2023-12-06 10:41:02 +0100

    chore: comments added

--- app/components/amplience/text/Text.tsx
+++ app/components/amplience/text/Text.tsx
@@ -1,6 +1,11 @@
 import type {ContentItem} from '~/clients/amplience/create-dc-content-client.types';
 import ReactMarkdown from 'markdown-to-jsx';
 
+/**
+ * Text component
+ * @param param0 object containing content data
+ * @returns Text component using markdown to HTML
+ */
 const Text = ({content}: {content: ContentItem}) => {
     return (
         <div className="markdown">
