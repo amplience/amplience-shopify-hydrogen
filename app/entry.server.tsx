d94dc2611caf8a6708007507d896f48cc866f696
Réza Kalfane <reza.kalfane@gmail.com>
2023-12-07 15:21:03 +0100

    fix: allowing ancestor (iframe viz)

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -10,7 +10,12 @@ export default async function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const {nonce, header, NonceProvider} = createContentSecurityPolicy();
+  const {nonce, header, NonceProvider} = createContentSecurityPolicy({
+    frameAncestors: [
+      "'self'",
+      'https://app.amplience.net',
+    ]
+  });
 
   const body = await renderToReadableStream(
     <NonceProvider>
