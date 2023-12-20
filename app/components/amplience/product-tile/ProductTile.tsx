b5818bdf46550000bcfc54e00f93eb72d0b28c89
Ben Pearey <bpearey@amplience.com>
2023-12-20 17:37:10 +0000

    fix(product tile): wrapping long product titles

--- app/components/amplience/product-tile/ProductTile.tsx
+++ app/components/amplience/product-tile/ProductTile.tsx
@@ -23,7 +23,7 @@ const ProductTile = ({title, image, price}: ProductTileProps) => {
         />
       )}
       <div className="absolute bottom-0 w-full">
-        <div className="m-2 p-2 bg-white/70 hover:bg-white/80 text-wrap rounded">
+        <div className="m-2 p-2 bg-white/70 hover:bg-white/80 rounded whitespace-normal">
           <h3 className="text-base font-[600]">{title}</h3>
           {price && <Money data={price} />}
         </div>
