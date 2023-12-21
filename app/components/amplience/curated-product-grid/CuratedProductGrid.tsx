6f00f3f95e0e3ce9fe4ae1a07668671831107114
Ben Pearey <bpearey@amplience.com>
2023-12-21 17:09:08 +0000

    refactor(product grid): using reusable product grid component

--- app/components/amplience/curated-product-grid/CuratedProductGrid.tsx
+++ app/components/amplience/curated-product-grid/CuratedProductGrid.tsx
@@ -1,10 +1,8 @@
-import {Link, useFetcher} from '@remix-run/react';
+import {useFetcher} from '@remix-run/react';
 import {useEffect, useState} from 'react';
 
-import SideScroller from '../side-scroller/SideScroller';
-import SideScrollerItem from '../side-scroller/SideScrollerItem';
-import ProductTile from '../product-tile/ProductTile';
 import {type Product} from '@shopify/hydrogen/storefront-api-types';
+import ProductGrid from '../product-grid/ProductGrid';
 
 type CuratedProductGridProps = {
   header: string;
@@ -15,8 +13,6 @@ const CuratedProductGrid = ({header, products}: CuratedProductGridProps) => {
   const fetcher = useFetcher();
   const [hydratedProducts, setHydratedProducts] = useState<Product[]>();
   const [isLoading, setIsLoading] = useState(true);
-
-  const hasProducts = (products?: Product[]) => products && products.length > 0;
 
   useEffect(() => {
     // set the hydrated products when the fetcher data changes
@@ -34,32 +30,16 @@ const CuratedProductGrid = ({header, products}: CuratedProductGridProps) => {
   }, [products]);
 
   return (
-    <div className="pt-8">
-      {header && (
-        <h2 className="text-center mt-4 mt-6 text-2xl md:text-4xl">{header}</h2>
-      )}
+    <>
       {isLoading && (
-        <div className="skeleton-placeholder h-80 w-[150px] m-auto"></div>
+        <div className="pt-8">
+          <div className="skeleton-placeholder h-80 w-[150px] m-auto"></div>
+        </div>
       )}
-      {!isLoading && hasProducts(hydratedProducts) && (
-        <SideScroller>
-          {hydratedProducts &&
-            hydratedProducts.map(
-              ({id, title, featuredImage, handle, variants}) => (
-                <SideScrollerItem key={id}>
-                  <Link to={`/products/${handle}`}>
-                    <ProductTile
-                      title={title}
-                      image={featuredImage}
-                      price={variants?.nodes[0]?.price}
-                    />
-                  </Link>
-                </SideScrollerItem>
-              ),
-            )}
-        </SideScroller>
+      {!isLoading && hydratedProducts && (
+        <ProductGrid header={header} products={hydratedProducts} />
       )}
-    </div>
+    </>
   );
 };
 
--- app/components/amplience/dynamic-product-grid/DynamicProductGrid.tsx
+++ app/components/amplience/dynamic-product-grid/DynamicProductGrid.tsx
@@ -1,12 +1,7 @@
-import {Link, useFetcher} from '@remix-run/react';
-import {
-  type Collection,
-  type Product,
-} from '@shopify/hydrogen/storefront-api-types';
+import {useFetcher} from '@remix-run/react';
+import {type Collection} from '@shopify/hydrogen/storefront-api-types';
 import {useEffect, useState} from 'react';
-import SideScroller from '../side-scroller/SideScroller';
-import SideScrollerItem from '../side-scroller/SideScrollerItem';
-import ProductTile from '../product-tile/ProductTile';
+import ProductGrid from '../product-grid/ProductGrid';
 
 export type DynamicProductGridProps = {
   header: string;
@@ -17,13 +12,11 @@ export type DynamicProductGridProps = {
 const DynamicProductGrid = ({
   header,
   category,
-  limit = 10,
+  limit,
 }: DynamicProductGridProps) => {
   const fetcher = useFetcher();
   const [collection, setCollection] = useState<Collection>();
   const [isLoading, setIsLoading] = useState(true);
-
-  const hasProducts = (products?: Product[]) => products && products.length > 0;
 
   useEffect(() => {
     // set the hydrated products when the fetcher data changes
@@ -35,40 +28,21 @@ const DynamicProductGrid = ({
 
   useEffect(() => {
     // load shopify product data when the products list changes
-    if (category) {
+    if (category && limit) {
       fetcher.load(`/api/collection?id=${category}&limit=${limit}`);
     }
   }, [category, limit]);
   return (
     <>
-      <div className="pt-8">
-        {header && (
-          <h2 className="text-center mt-4 mt-6 text-2xl md:text-4xl">
-            {header}
-          </h2>
-        )}
-        {isLoading && (
+      {isLoading && (
+        <div className="pt-8">
           <div className="skeleton-placeholder h-80 w-[150px] m-auto"></div>
-        )}
+        </div>
+      )}
 
-        {!isLoading && hasProducts(collection?.products?.nodes) && (
-          <SideScroller>
-            {collection?.products?.nodes.map(
-              ({id, title, featuredImage, handle, variants}) => (
-                <SideScrollerItem key={id}>
-                  <Link to={`/products/${handle}`}>
-                    <ProductTile
-                      title={title}
-                      image={featuredImage}
-                      price={variants?.nodes[0]?.price}
-                    />
-                  </Link>
-                </SideScrollerItem>
-              ),
-            )}
-          </SideScroller>
-        )}
-      </div>
+      {!isLoading && collection?.products?.nodes && (
+        <ProductGrid header={header} products={collection?.products?.nodes} />
+      )}
     </>
   );
 };
