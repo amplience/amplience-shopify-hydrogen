9676167804440cf65178317db4a0c6e11678efa6
Réza Kalfane <reza.kalfane@gmail.com>
2023-12-28 16:56:12 +0100

    chore: tailwind styling

--- app/components/amplience/debug-panel/DebugPanel.tsx
+++ app/components/amplience/debug-panel/DebugPanel.tsx
@@ -1,6 +1,5 @@
-import { useLoaderData } from "@remix-run/react";
-import { LoaderFunctionArgs, defer } from "@shopify/remix-oxygen";
 import { useState } from "react";
+import AmplienceLogo from "../../../assets/svg/amp-logo.svg";
 
 export type DebugPanelProps = {
   hubName: string;
@@ -18,11 +17,11 @@ const DebugPanel = ({
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex justify-end p-4">
+    <>
       {
         stagingHost &&
         (
-          <>
+          <div className="flex justify-end p-2 bg-orange-200">
             <button
               className="bg-orange-500 text-white rounded px-4 py-1"
               onClick={() => setIsOpen(true)}
@@ -48,16 +47,34 @@ const DebugPanel = ({
                     onClick={() => {
                       setIsOpen(false);
                     }}
-                    className="p-4 font-bold text-lg">
-                    Header
+                    className="p-4 font-bold text-lg mx-auto">
+                    <img src={AmplienceLogo} alt="Amplience Logo" width="260" />
                   </header>
-                  <div className="p-4">
-                    <h2>Preview</h2>
-                    <h2>Environment</h2>
-                    {locale && <p className="text-xs">Locale: <b>{locale}</b></p>}
-                    {hubName && <p className="text-xs">Hub Name: <b>{hubName}</b></p>}
-                    {stagingHost && <p className="text-xs">Staging Host: <b>{stagingHost}</b></p>}
-                    {contentId && <p className="text-xs">Content ID: <b>{contentId}</b></p>}
+                  <div className="flex items-center justify-center ">
+                    <div className="w-full mx-auto space-y-2  lg:max-w-md">
+                      <details className="p-2 rounded-lg">
+                        <summary className="font-semibold bg-orange-200 px-4 py-2 text-lg cursor-pointer">Preview</summary>
+                        <div className=" bg-orange-100">
+                          <p className="leading-6 px-3 py-2 text-gray-800">
+                          </p>
+                        </div>
+                      </details>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-center ">
+                    <div className="w-full mx-auto space-y-2  lg:max-w-md">
+                      <details className="p-2 rounded-lg">
+                        <summary className="font-semibold bg-orange-200 px-4 py-2 text-lg cursor-pointer">Environment</summary>
+                        <div className=" bg-orange-100">
+                          <p className="leading-6 px-3 py-2 text-gray-800">
+                            {locale && <p className="text-xs">Locale: <b>{locale}</b></p>}
+                            {hubName && <p className="text-xs">Hub Name: <b>{hubName}</b></p>}
+                            {stagingHost && <p className="text-xs">Staging Host: <b>{stagingHost}</b></p>}
+                            {contentId && <p className="text-xs">Content ID: <b>{contentId}</b></p>}
+                          </p>
+                        </div>
+                      </details>
+                    </div>
                   </div>
                 </article>
               </section>
@@ -68,9 +85,10 @@ const DebugPanel = ({
                 }}
               ></section>
             </div>
-          </>)
+          </div>
+        )
       }
-    </div>
+    </>
   );
 };
 
