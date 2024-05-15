eab22c3961f2673584c28292432f4ebea9995d58
Réza Kalfane <reza.kalfane@gmail.com>
2024-05-15 11:43:52 +0200

    fix: di image implementation

--- app/components/amplience/card/Card.tsx
+++ app/components/amplience/card/Card.tsx
@@ -1,19 +1,88 @@
 import {type AmplienceContentItem} from '~/clients/amplience/fetch-content';
 import AmplienceContent from '../wrapper/AmplienceContent';
+import {
+  AmplienceImage,
+  ImageScaleFit,
+  ImageScaleMode,
+  ImageTransformations,
+} from '../image/Image.types';
+import {useEffect, useRef, useState} from 'react';
+import DefaultAdaptiveImageSkeleton from '../adaptive-image/DefaultAdaptiveImageSkeleton';
+import DefaultAdaptiveImageRef from '../adaptive-image/DefaultAdaptiveImage';
 
 export type CardProps = {
   className?: string;
-  image?: AmplienceContentItem;
+  image: {
+    img: {
+      image: ImageTransformations & {
+        image: AmplienceImage;
+      };
+    };
+    disablePoiAspectRatio: boolean;
+    imageAltText: string;
+    di: string;
+  };
   cardName?: string;
   description?: string;
   links?: any[];
 };
 
 const Card = ({image, cardName, description, links}: CardProps) => {
+  const [imageLoading, setImageLoading] = useState(true);
+  const imageRef = useRef<any>();
+
+  /**
+   * Method called with the image is loaded
+   */
+  const handleImageLoaded = () => {
+    setImageLoading(false);
+  };
+
+  /**
+   * Checking that the image is loaded
+   */
+  useEffect(() => {
+    if (imageRef?.current?.complete && imageLoading) {
+      setImageLoading(false);
+    }
+  }, [imageRef?.current?.complete, imageLoading]);
+
+  const {img} = image || {};
+
+  const transformations: ImageTransformations = {
+    ...img?.image,
+    upscale: false,
+    strip: true,
+    quality: 80,
+    scaleMode: !image?.disablePoiAspectRatio
+      ? ImageScaleMode.ASPECT_RATIO
+      : undefined,
+    scaleFit:
+      !image?.disablePoiAspectRatio &&
+      img?.image?.poi &&
+      img?.image?.poi.x != -1 &&
+      img?.image?.poi.y != -1
+        ? ImageScaleFit.POINT_OF_INTEREST
+        : undefined,
+  };
+
   return (
     <div className="w-full m-[4px]">
-      <div>
-        <AmplienceContent content={image as AmplienceContentItem} />
+      {imageLoading ? <DefaultAdaptiveImageSkeleton /> : null}
+      <div
+        className="bg-[#ccc]"
+        style={{
+          display: `${imageLoading ? 'none' : 'block'}`,
+        }}
+      >
+        <DefaultAdaptiveImageRef
+          ref={imageRef}
+          onLoad={() => handleImageLoaded()}
+          image={img?.image.image}
+          imageAltText={image?.imageAltText}
+          transformations={transformations}
+          diParams={image?.di}
+        />
       </div>
       <h3>{cardName}</h3>
       <p>{description}</p>
