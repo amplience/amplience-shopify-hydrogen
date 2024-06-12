1e93a499766746f89498fe114ec4db09518fa77d
Réza Kalfane <reza.kalfane@gmail.com>
2024-06-12 17:04:19 +0200

    chore: updated styles

--- app/components/amplience/rich-text/RichText.tsx
+++ app/components/amplience/rich-text/RichText.tsx
@@ -30,61 +30,6 @@ type RichTextProps = {
 };
 
 const RichText = ({text, align = 'left', header}: RichTextProps) => {
-  const options = {
-    overrides: {
-      a: {component: Link},
-      h1: {
-        component: ({...props}) => (
-          <h1 className="mb-4 mt-4 text-3xl font-black lg:text-5xl">
-            <span {...props} />
-          </h1>
-        ),
-      },
-      h2: {
-        component: ({...props}) => (
-          <h2 className="mb-4 mt-4 text-2xl font-black lg:text-4xl">
-            <span {...props} />
-          </h2>
-        ),
-      },
-      h3: {
-        component: ({...props}) => (
-          <h3 className="mb-4 mt-4 text-xl font-black lg:text-3xl">
-            <span {...props} />
-          </h3>
-        ),
-      },
-      h4: {
-        component: ({...props}) => (
-          <h4 className="mb-4 mt-4 text-xl font-black lg:text-2xl">
-            <span {...props} />
-          </h4>
-        ),
-      },
-      h5: {
-        component: ({...props}) => (
-          <h5 className="mb-4 mt-4 text-xl font-black lg:text-xl">
-            <span {...props} />
-          </h5>
-        ),
-      },
-      h6: {
-        component: ({...props}) => (
-          <h5 className="mb-4 mt-4 font-black">
-            <span {...props} />
-          </h5>
-        ),
-      },
-      li: {
-        component: ({...props}) => (
-          <li>
-            <span {...props} />
-          </li>
-        ),
-      },
-    },
-  };
-
   return (
     <div
       className="[&_ul]:block [&_ul]:list-disc [&_ul]:ps-[40px] [&_ul]:ms-[0px] [&_ul]:me-[0px] [&_img]:w-full [&_p]:my-2.5"
@@ -100,9 +45,7 @@ const RichText = ({text, align = 'left', header}: RichTextProps) => {
             case 'markdown':
               return (
                 <div key={index} className="my-5" style={{textAlign: align}}>
-                  {data && (
-                    <ReactMarkdown options={options}>{data}</ReactMarkdown>
-                  )}
+                  {data && <ReactMarkdown>{data}</ReactMarkdown>}
                 </div>
               );
             case 'dc-content-link':
